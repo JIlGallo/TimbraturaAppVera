43bc2b8782a5b34d73dcb69c919f5e5a26c906ac
Emanuele <Emanuele@LAB-EMANUELE>
2024-09-27 17:31:32 +0200

    Aggiungere i file di progetto.

---
+++ TimbratureApp/Service/TimbratureService.cs
@@ -0,0 +1,15 @@
+﻿namespace TimbratureApp.Service
+{
+    public class TimbratureService
+    {
+        public List<TimbraturaPersona> Timbrature { get; } = new List<TimbraturaPersona>();
+    }
+
+    public class TimbraturaPersona
+    {
+        public DateTime DataOra { get; set; }
+        public double Latitudine { get; set; }
+        public double Longitudine { get; set; }
+        public int IdUtente { get; set; }
+    }
+}
